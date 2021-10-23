e796ce16d455f042ac9d04e5255c932f0b390f31
paulni123 <nitin.paul1530@gmail.com>
2021-10-23 09:34:01 -0400

    footer is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 import Google from './GoogleMaps/Google';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Switch>
-        <Route path='/' exact component={Navbar} />
+        <Route path='/' exact component={HomePage} />
         <Route path='/explore' exact component={Google} />
         {/* <Route path='/login' exact component={LoginPage} /> */}
         <Route path='/' render={() => <div>404 Not Found</div>} />
